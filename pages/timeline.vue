a954293235f7d843f69a3d32ff0701aca311ca70
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2019-12-08 23:06:38 +0900

    タイムライン途中2

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -15,26 +15,26 @@
         .hero-body
             .section
                 ul
-                    li(v-for="_ in 10", style={'margin-bottom': '1rem'})
+                    li(v-for="post in posts", style={'margin-bottom': '1rem'})
                         .box
                             article.media
                                 .media-left
                                     figure.image.is-64x64
-                                        img(src="https://bulma.io/images/placeholders/128x128.png")
+                                        img(:src="post.user.imageurl")
                                 .media-content
                                     .content
                                         p
-                                            strong name &ensp; 
-                                            small time
+                                            strong {{ post.user.name }} &nbsp;
+                                            small {{ post.createAt }}
+                                            br 
+                                            | {{ post.spot.locales[$i18n.locale].name }}
                                         client-only
                                             figure(v-lazy-container="{ selector: 'img' }") 
-                                                img(:data-src="'https://i.imgur.com/JOoDeYg.jpg'", 
+                                                img(:data-src="post.imageurl", 
                                                     data-loading="/images/preloader.gif")
                                         p 
-                                            | 本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文
-                                            | 本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文
-                                            | 本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文
-                infinite-loading(spinner="spiral", @infinite="infiniteHandler")
+                                            | {{ post.description }}
+                    infinite-loading(spinner="spiral", @infinite="infiniteHandler")
 
 </template>
 
@@ -44,7 +44,33 @@ import InfiniteLoading from 'vue-infinite-loading';
 export default {
     data() {
         return {
-
+            posts: [
+                {
+                    id: 1,
+                    user: {
+                        id: 1,
+                        name: 'admin',
+                        imageurl: 'https://i.imgur.com/0U0MMRT.jpg', 
+                    },
+                    imageurl: 'https://i.imgur.com/JOoDeYg.jpg',
+                    description: '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文',
+                    spot: {
+                        id: 1,
+                        img_url: "https://i.imgur.com/c7glCUA.jpg",
+                        locales: {
+                            en: {
+                                name: "Higashi chaya",
+                                discription: "Kenrokuen is one of the best gardens in Japan"
+                            },
+                            ja: {
+                                name: "東茶屋街",
+                                discription: "金沢の東側にある古くからある歓楽街です"
+                            }
+                        }
+                    },
+                    createAt: '2019-12-8',
+                },
+            ]
         }
     },
     fetch ({ app, store, redirect }) {
