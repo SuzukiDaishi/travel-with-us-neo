ce9d1e68f40fe1b422b6b1da6fd11a17d95ca874
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2019-12-10 05:40:07 +0900

    ポストのフェードを実装した

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -10,10 +10,14 @@
                         .buttons
                             button.button.has-text-centerd(style={'border-radius': '50%', 
                                                                 　'border-color': 'black',
-                                                                　'border-width': '2px'})
-                                i.fa.fa-plus
+                                                                　'border-width': '2px'},
+                                                           @click="postButtonClick()")
+                                i#plusIcon.fa.fa-plus
         .hero-body
             .section
+                transition
+                    .box(v-if="isPosting")
+                        | hello
                 ul
                     li(v-for="post in posts", style={'margin-bottom': '1rem'})
                         .box
@@ -39,12 +43,14 @@
 </template>
 
 <script>
-import InfiniteLoading from 'vue-infinite-loading';
+import InfiniteLoading from 'vue-infinite-loading'
+import anime from 'animejs'
 
 export default {
     data() {
         return {
             isLoading: false,
+            isPosting: false,
             posts: []
         }
     },
@@ -67,7 +73,30 @@ export default {
                 $state.loaded()
                 this.isLoading = false
             })
+        },
+        postButtonClick() {
+            this.isPosting = !this.isPosting
+            if (this.isPosting) {
+                anime({
+                    targets: '#plusIcon',
+                    rotate:  45
+                })
+            } else {
+                anime({
+                    targets: '#plusIcon',
+                    rotate: 0
+                })
+            }
         }
     }
 }
 </script>
+
+<style lang="sass">
+.v-enter-active
+    transition: opacity 0.5s
+.v-leave-active
+    transition: opacity 0.2s
+.v-enter, .v-leave-to
+    opacity: 0
+</style>
