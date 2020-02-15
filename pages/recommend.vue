fb7ff5ff8db9643f2dab36c716170ae76706fd96
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2020-02-15 11:05:05 +0900

    リファクタリングした

--- pages/recommend.vue
+++ pages/recommend.vue
@@ -19,7 +19,7 @@
                                 .column.is-two-fifths
                                     google-map(:gmapIframe="spotLocations.filter( s => s.id === spot.id )[0].map")        
                             span
-                                nuxt-link(to="/").button {{ $t('RECOMMEND_DETAILS') }}
+                                nuxt-link(:to="localePath('index')").button {{ $t('RECOMMEND_DETAILS') }}
 </template>
 
 <script>
@@ -45,88 +45,85 @@ export default {
         }
     },
     methods: {
-        popularSortPreData(spots, posts) {
-            return spots
-                .sort( (a, b) => a.id > b.id )
-                .map( s => s.id)
-                .map( i => {
-                    return {
-                        id: i,
-                        count: posts.filter(p=>p.spot.id==i).length
-                    }
-                })
-        },
-        locationSortPreData(spots, location) {
-            return spots
-                .sort( (a, b) => a.id > b.id )
-                .map( s => s.id)
-                .map( i => this.spotLocations.filter( l => l.id == i )[0] )
-                .map( l => { 
-                    return {
-                        latitude: l.latitude,
-                        longitude: l.longitude
-                    }
-                })
-                .map( l => greatCircleDistance({
-                    lat1: `${ l.latitude }`,
-                    lng1: `${ l.longitude }`,
-                    lat2: `${ location.latitude }`,
-                    lng2: `${ location.longitude }`,
-                }) )
-                .map( (d, i) => {
-                    return {
-                        id: i,
-                        distance: d
-                    }
-                } )
-        },
-        recommendData(spots, popData, locData) {
-            return spots
-                .sort( (a, b) => a.id > b.id )
+        postsPoint(posts) {
+            const postIds = posts
+                .map( p => p.spot.id )
+                .filter( i => i!==-1 )
+            return this.spots
                 .map( s => s.id )
-                .map( i => {
-                    // TODO: ここはあとで調整
-                    let p = popData.filter( p => p.id == i )[0].count 
-                    p /= popData.length
-                    let l = locData.filter( l => l.id == i )[0].distance
-                    l = - Math.tanh(l*3-3) // 1km人の歩ける距離は1km程度かなと思った(それ以上はマイナス要因)
-                    console.log({ i, l, p, point: ( p + l ) / 2 })
-                    return ( p + l ) / 2
-                } )
-                .map( (p, i) => {
+                .map( i => { 
                     return {
                         id: i,
-                        point: p
+                        point: postIds.filter( p => p === i ).length / postIds.length
+                    }
+                })
+        },
+        async locationsPoint() {
+            const getLocations = async () => {
+                if ( ! ( "geolocation" in navigator ) ) { throw Error('CAN NOT USE GEOLOCATION') }
+                return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject, {}) )
+                    .then( e => e.coords ) 
+                    .then( e => {
+                        return {
+                            latitude: e.latitude,
+                            longitude: e.longitude
+                        }
+                    })
+            }
+            const myPlace = await getLocations()
+            return this.spotLocations
+                .map( s => {
+                    return {
+                        id: s.id,
+                        latitude: s.latitude,
+                        longitude: s.longitude
+                    }
+                })
+                .map( l => {
+                    return {
+                        id: l.id,
+                        distance: greatCircleDistance({
+                            lat1: `${l.latitude}`,
+                            lng1: `${l.longitude}`,
+                            lat2: `${myPlace.latitude}`,
+                            lng2: `${myPlace.longitude}`,
+                        })
+                    }
+                })
+                .map( l => {
+                    return {
+                        id: l.id,
+                        point: - Math.tanh( 3 * l.distance - 3 ) / 3 + 0.5
                     }
                 } )
+        },
+        spotsSorted(spotsPoint) {
+            this.spots = this.spots
+                .sort( (a, b) => {
+                    const aPoint = spotsPoint.filter( i => i.id == a.id )[0].point
+                    const bPoint = spotsPoint.filter( i => i.id == b.id )[0].point
+                    return aPoint < bPoint ? 1 : -1
+                })
         }
     },
-    mounted() {
-        this.$axios
-        .get(urljoin(process.env.baseUrl,`/api/spots/show`))
-        .then( res => res.data.data )
-        .then( posts => {
-            const popPreData = this.popularSortPreData(this.spots, posts)
-            this.spots = this.spots.sort( (a, b) => {
-                const flag = popPreData.filter( i => i.id == a.id )[0].count >
-                    popPreData.filter( i => i.id == b.id )[0].count
-                return flag ? -1 : 1
-            })
-            if ("geolocation" in navigator) {
-                navigator.geolocation.getCurrentPosition((pos) => {
-                    const locPreData = this.locationSortPreData(this.spots, {
-                        latitude: pos.coords.latitude,
-                        longitude: pos.coords.longitude
-                    })
-                    const pointData = this.recommendData(this.spots, popPreData, locPreData)
-                    this.spots = this.spots.sort( (a, b) => {
-                        const flag = pointData.filter( i => i.id == a.id )[0].point >
-                            pointData.filter( i => i.id == b.id )[0].point
-                        return flag ? -1 : 1
-                    })
+    async mounted() {
+        try {
+            let postPoints = await this.$axios.get(urljoin(process.env.baseUrl,`/api/spots/show`))
+            postPoints = postPoints.data.data
+            postPoints = this.postsPoint(postPoints)
+            let locationPoints = await this.locationsPoint()
+            let point = this.spots.map( i => i.id )
+                .map(i => {
+                    return {
+                        id: i,
+                        point: (postPoints.filter(p=>p.id===i)[0].point+locationPoints.filter(p=>p.id===i)[0].point)/2
+                    }
                 })
-            } 
-        })
+            this.spotsSorted(point)
+            console.log(postPoints, locationPoints, point)
+        } catch(e) {
+            console.log('error', e)
+        }
     }
 }
 </script>
