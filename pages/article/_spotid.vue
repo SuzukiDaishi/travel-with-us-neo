970aeffe1f283947d1aa75d8bd98651ebe60c4b4
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2020-02-19 12:18:19 +0900

    記事ページ(PCのみ)を作った

--- pages/article/_spotid.vue
+++ pages/article/_spotid.vue
@@ -6,14 +6,38 @@
         </header>
         <div>
             
-            <!-- is PC or Tablet -->
-            <div v-if="$device.isDesktopOrTablet">
-                <iframe :src="spotsNews.filter(i=>i.id==$route.params.spotid)[0][$i18n.locale].path"></iframe>
-            </div>
+            <!-- is PC -->
+            <section class="container top-space" v-if="$device.isDesktop">
+                <div class="columns">
+                    <div class="column is-9 height-box">
+                        <iframe :src="spotsNews.filter(i=>i.id==$route.params.spotid)[0][$i18n.locale].path"></iframe>
+                    </div>
+                    <div class="column is-3" >
+                        <div class="container box-wrapper height-box box-scroll">
+                            <div class="box box-small" v-for="(post, index) in spotPosts($route.params.spotid)" :key="index">
+                                <article class="media">
+                                    <div class="media-content">
+                                        <div class="content">
+                                            <p>
+                                                <strong> {{ post.user.name }} </strong> 
+                                                <br>
+                                                <img :src="post.imageurl">
+                                                <br>
+                                                {{ post.text }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-            <!-- is Mobile -->
-            <div v-else>
-            </div>
+            <!-- is Mobile or Tablet -->
+            <section v-else>
+                卍できてないよ卍
+            </section>
 
         </div>
     </div>
@@ -29,18 +53,43 @@ export default {
     },
     async mounted() {
         let posts = await this.$axios.get(urljoin(process.env.baseUrl,`/api/spots/show`))
-        console.log(posts.data.data);
+        this.posts = posts.data.data
     },
     data() {
         return {
             spotsNews: require('@/assets/data/spotsNews.json'),
+            posts: []
         }
     },
+    methods: {
+        spotPosts(spotid) {
+            return this.posts.filter(p=>p.spot.id==spotid)
+        }
+    }
 }
 </script>
 
 <style>
 #header-space {
     padding-bottom: 52px;
+}
+iframe {
+    width: 100%;
+    height: 100%;
+}
+.top-space {
+    margin-top: 20px;
+}
+.height-box {
+    height: calc(100vh - 92px );
+}
+.box-scroll {
+    overflow-y: scroll;
+}
+.box-small {
+    width: 90%;
+}
+.box-wrapper {
+    padding: 2px 5px;
 }
 </style>
